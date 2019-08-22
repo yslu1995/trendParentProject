b376ac878aa5b125a318d4e9cc7a104999024a57
yslu <469899431@qq.com>
2019-08-22 23:51:24 +0800

    redis

--- index-gather-store-service/src/main/java/cn/how2j/trend/service/IndexService.java
+++ index-gather-store-service/src/main/java/cn/how2j/trend/service/IndexService.java
@@ -4,6 +4,8 @@ import cn.how2j.trend.pojo.Index;
 import cn.hutool.core.collection.CollectionUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
  
@@ -16,6 +18,8 @@ import java.util.Map;
  * http://127.0.0.1:8090/indexes/codes.json
  */
 @Service
+//保存到 redis 就会以 indexes 命名
+@CacheConfig(cacheNames="indexes")
 public class IndexService {
     private List<Index> indexes;
     @Autowired RestTemplate restTemplate;
@@ -27,6 +31,8 @@ public class IndexService {
      * http://127.0.0.1:8001/getCodes
      */
     @HystrixCommand(fallbackMethod = "third_part_not_connected")
+    //表示保存到 redis 用的 key 就会使 all_codes.
+    @Cacheable(key="'all_codes'")
     public List<Index> fetch_indexes_from_third_part(){
         List<Map> temp= restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json",List.class);
         return map2Index(temp);
