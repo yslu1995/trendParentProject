cb2dfa9e1fc60bda5c4a1f873ad41e44846daff4
yslu <469899431@qq.com>
2019-09-20 21:48:48 +0800

    模拟回测-交易统计

--- trend-trading-backtest-service/src/main/java/cn/how2j/trend/service/BackTestService.java
+++ trend-trading-backtest-service/src/main/java/cn/how2j/trend/service/BackTestService.java
@@ -13,9 +13,10 @@ import java.util.*;
 
 @Service
 public class BackTestService {
-    @Autowired IndexDataClient indexDataClient;
+    @Autowired
+    IndexDataClient indexDataClient;
 
-    public List<IndexData> listIndexData(String code){
+    public List<IndexData> listIndexData(String code) {
         List<IndexData> result = indexDataClient.getIndexData(code);
         Collections.reverse(result);
 
@@ -25,9 +26,10 @@ public class BackTestService {
 
         return result;
     }
-    public Map<String,Object> simulate(int ma, float sellRate, float buyRate, float serviceCharge, List<IndexData> indexDatas)  {
 
-        List<Profit> profits =new ArrayList<>();
+    public Map<String, Object> simulate(int ma, float sellRate, float buyRate, float serviceCharge, List<IndexData> indexDatas) {
+
+        List<Profit> profits = new ArrayList<>();
         List<Trade> trades = new ArrayList<>();
 
         float initCash = 1000;
@@ -35,25 +37,31 @@ public class BackTestService {
         float share = 0;
         float value = 0;
 
-        float init =0;
-        if(!indexDatas.isEmpty()) {
-            init = indexDatas.get(0).getClosePoint();
-        }
+        int winCount = 0;
+        float totalWinRate = 0;
+        float avgWinRate = 0;
+        float totalLossRate = 0;
+        int lossCount = 0;
+        float avgLossRate = 0;
 
-        for (int i = 0; i<indexDatas.size() ; i++) {
+        float init = 0;
+        if (!indexDatas.isEmpty())
+            init = indexDatas.get(0).getClosePoint();
+
+        for (int i = 0; i < indexDatas.size(); i++) {
             IndexData indexData = indexDatas.get(i);
             float closePoint = indexData.getClosePoint();
-            float avg = getMA(i,ma,indexDatas);
-            float max = getMax(i,ma,indexDatas);
+            float avg = getMA(i, ma, indexDatas);
+            float max = getMax(i, ma, indexDatas);
 
-            float increase_rate = closePoint/avg;
-            float decrease_rate = closePoint/max;
+            float increase_rate = closePoint / avg;
+            float decrease_rate = closePoint / max;
 
-            if(avg!=0) {
+            if (avg != 0) {
                 //buy 超过了均线
-                if(increase_rate>buyRate  ) {
+                if (increase_rate > buyRate) {
                     //如果没买
-                    if(0 == share) {
+                    if (0 == share) {
                         share = cash / closePoint;
                         cash = 0;
 
@@ -66,61 +74,77 @@ public class BackTestService {
                     }
                 }
                 //sell 低于了卖点
-                else if(decrease_rate<sellRate ) {
+                else if (decrease_rate < sellRate) {
                     //如果没卖
-                    if(0!= share){
-                        cash = closePoint * share * (1-serviceCharge);
+                    if (0 != share) {
+                        cash = closePoint * share * (1 - serviceCharge);
                         share = 0;
 
-                        Trade trade =trades.get(trades.size()-1);
+                        Trade trade = trades.get(trades.size() - 1);
                         trade.setSellDate(indexData.getDate());
                         trade.setSellClosePoint(indexData.getClosePoint());
 
                         float rate = cash / initCash;
                         trade.setRate(rate);
+
+                        if (trade.getSellClosePoint() - trade.getBuyClosePoint() > 0) {
+                            totalWinRate += (trade.getSellClosePoint() - trade.getBuyClosePoint()) / trade.getBuyClosePoint();
+                            winCount++;
+                        } else {
+                            totalLossRate += (trade.getSellClosePoint() - trade.getBuyClosePoint()) / trade.getBuyClosePoint();
+                            lossCount++;
+                        }
                     }
                 }
                 //do nothing
-                else{
+                else {
 
                 }
             }
 
-            if(share!=0) {
+            if (share != 0) {
                 value = closePoint * share;
-            }
-            else {
+            } else {
                 value = cash;
             }
-            float rate = value/initCash;
+            float rate = value / initCash;
 
             Profit profit = new Profit();
             profit.setDate(indexData.getDate());
-            profit.setValue(rate*init);
+            profit.setValue(rate * init);
 
             profits.add(profit);
 
         }
-        Map<String,Object> map = new HashMap<>();
+
+        avgWinRate = totalWinRate / winCount;
+        avgLossRate = totalLossRate / lossCount;
+
+        Map<String, Object> map = new HashMap<>();
         map.put("profits", profits);
         map.put("trades", trades);
+
+        map.put("winCount", winCount);
+        map.put("lossCount", lossCount);
+        map.put("avgWinRate", avgWinRate);
+        map.put("avgLossRate", avgLossRate);
+
         return map;
     }
 
     private static float getMax(int i, int day, List<IndexData> list) {
-        int start = i-1-day;
-        if(start<0) {
+        int start = i - 1 - day;
+        if (start < 0)
             start = 0;
-        }
-        int now = i-1;
+        int now = i - 1;
 
-        if(start<0)
+        if (start < 0)
             return 0;
 
         float max = 0;
         for (int j = start; j < now; j++) {
-            IndexData bean =list.get(j);
-            if(bean.getClosePoint()>max) {
+            IndexData bean = list.get(j);
+            if (bean.getClosePoint() > max) {
                 max = bean.getClosePoint();
             }
         }
@@ -128,17 +152,16 @@ public class BackTestService {
     }
 
     private static float getMA(int i, int ma, List<IndexData> list) {
-        int start = i-1-ma;
-        int now = i-1;
+        int start = i - 1 - ma;
+        int now = i - 1;
 
-        if(start<0) {
+        if (start < 0)
             return 0;
-        }
 
         float sum = 0;
         float avg = 0;
         for (int j = start; j < now; j++) {
-            IndexData bean =list.get(j);
+            IndexData bean = list.get(j);
             sum += bean.getClosePoint();
         }
         avg = sum / (now - start);
@@ -148,13 +171,13 @@ public class BackTestService {
     public float getYear(List<IndexData> allIndexDatas) {
         float years;
         String sDateStart = allIndexDatas.get(0).getDate();
-        String sDateEnd = allIndexDatas.get(allIndexDatas.size()-1).getDate();
+        String sDateEnd = allIndexDatas.get(allIndexDatas.size() - 1).getDate();
 
         Date dateStart = DateUtil.parse(sDateStart);
         Date dateEnd = DateUtil.parse(sDateEnd);
 
         long days = DateUtil.between(dateStart, dateEnd, DateUnit.DAY);
-        years = days/365f;
+        years = days / 365f;
         return years;
     }
 
--- trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
@@ -17,37 +17,43 @@ import java.util.*;
 
 @RestController
 public class BackTestController {
-    @Autowired BackTestService backTestService;
+    @Autowired
+    BackTestService backTestService;
 
     @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
-    public Map<String,Object> backTest(
+    public Map<String, Object> backTest(
             @PathVariable("code") String code
-            ,@PathVariable("startDate") String strStartDate
-            ,@PathVariable("endDate") String strEndDate
+            , @PathVariable("startDate") String strStartDate
+            , @PathVariable("endDate") String strEndDate
     ) throws Exception {
         List<IndexData> allIndexDatas = backTestService.listIndexData(code);
 
         String indexStartDate = allIndexDatas.get(0).getDate();
-        String indexEndDate = allIndexDatas.get(allIndexDatas.size()-1).getDate();
+        String indexEndDate = allIndexDatas.get(allIndexDatas.size() - 1).getDate();
 
-        allIndexDatas = filterByDateRange(allIndexDatas,strStartDate, strEndDate);
+        allIndexDatas = filterByDateRange(allIndexDatas, strStartDate, strEndDate);
 
         int ma = 20;
         float sellRate = 0.95f;
         float buyRate = 1.05f;
         float serviceCharge = 0f;
-        Map<String,?> simulateResult= backTestService.simulate(ma,sellRate, buyRate,serviceCharge, allIndexDatas);
+        Map<String, ?> simulateResult = backTestService.simulate(ma, sellRate, buyRate, serviceCharge, allIndexDatas);
         List<Profit> profits = (List<Profit>) simulateResult.get("profits");
         List<Trade> trades = (List<Trade>) simulateResult.get("trades");
 
         float years = backTestService.getYear(allIndexDatas);
-        float indexIncomeTotal = (allIndexDatas.get(allIndexDatas.size()-1).getClosePoint() - allIndexDatas.get(0).getClosePoint()) / allIndexDatas.get(0).getClosePoint();
-        float indexIncomeAnnual = (float) Math.pow(1+indexIncomeTotal, 1/years) - 1;
-        float trendIncomeTotal = (profits.get(profits.size()-1).getValue() - profits.get(0).getValue()) / profits.get(0).getValue();
-        float trendIncomeAnnual = (float) Math.pow(1+trendIncomeTotal, 1/years) - 1;
+        float indexIncomeTotal = (allIndexDatas.get(allIndexDatas.size() - 1).getClosePoint() - allIndexDatas.get(0).getClosePoint()) / allIndexDatas.get(0).getClosePoint();
+        float indexIncomeAnnual = (float) Math.pow(1 + indexIncomeTotal, 1 / years) - 1;
+        float trendIncomeTotal = (profits.get(profits.size() - 1).getValue() - profits.get(0).getValue()) / profits.get(0).getValue();
+        float trendIncomeAnnual = (float) Math.pow(1 + trendIncomeTotal, 1 / years) - 1;
 
-        Map<String,Object> result = new HashMap<>();
+        int winCount = (Integer) simulateResult.get("winCount");
+        int lossCount = (Integer) simulateResult.get("lossCount");
+        float avgWinRate = (Float) simulateResult.get("avgWinRate");
+        float avgLossRate = (Float) simulateResult.get("avgLossRate");
+
+        Map<String, Object> result = new HashMap<>();
         result.put("indexDatas", allIndexDatas);
         result.put("indexStartDate", indexStartDate);
         result.put("indexEndDate", indexEndDate);
@@ -59,11 +65,16 @@ public class BackTestController {
         result.put("trendIncomeTotal", trendIncomeTotal);
         result.put("trendIncomeAnnual", trendIncomeAnnual);
 
+        result.put("winCount", winCount);
+        result.put("lossCount", lossCount);
+        result.put("avgWinRate", avgWinRate);
+        result.put("avgLossRate", avgLossRate);
+
         return result;
     }
 
     private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
-        if(StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate) ) {
+        if (StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate)) {
             return allIndexDatas;
         }
 
@@ -72,10 +83,10 @@ public class BackTestController {
         Date endDate = DateUtil.parse(strEndDate);
 
         for (IndexData indexData : allIndexDatas) {
-            Date date =DateUtil.parse(indexData.getDate());
-            if(
-                    date.getTime()>=startDate.getTime() &&
-                            date.getTime()<=endDate.getTime()
+            Date date = DateUtil.parse(indexData.getDate());
+            if (
+                    date.getTime() >= startDate.getTime() &&
+                            date.getTime() <= endDate.getTime()
             ) {
                 result.add(indexData);
             }
