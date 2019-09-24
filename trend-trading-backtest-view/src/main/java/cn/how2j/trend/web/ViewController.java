df686afae7018907cbf46061be0c9579e24bad83
yslu <469899431@qq.com>
2019-09-24 10:58:09 +0800

    服务器客户端

--- trend-trading-backtest-view/src/main/java/cn/how2j/trend/web/ViewController.java
+++ trend-trading-backtest-view/src/main/java/cn/how2j/trend/web/ViewController.java
@@ -1,14 +1,17 @@
 package cn.how2j.trend.web;
- 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-
+    @Value("${version}")
+    String version;
     @GetMapping("/")
-    public String view() throws Exception {
-        System.out.println("当前使用的service");
+    public String view(Model m) throws Exception {
+        m.addAttribute("version", version);
         return "view";
     }
 }
